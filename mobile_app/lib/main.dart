666cd70e343bbfabb7b095831489da93f0a9d97f
zhammer <zachary_hammer@alumni.brown.edu>
2020-11-10 17:19:37 -0500

    change primary colour of cupertinou theme

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -6,13 +6,17 @@ void main() {
   runApp(App());
 }
 
+const _leaf0 = Color.fromRGBO(200, 47, 49, 1);
+
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
       title: 'north',
-      theme: CupertinoThemeData().copyWith(),
+      theme: CupertinoThemeData().copyWith(
+        primaryColor: _leaf0,
+      ),
       home: LandingPage(title: 'north'),
     );
   }
