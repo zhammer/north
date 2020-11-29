1d49952a7570b1732656e32db94334e944b66da9
zhammer <zachary_hammer@alumni.brown.edu>
2020-11-29 15:29:35 -0500

    start working on haiku carousel

--- mobile_app/lib/widgets/post.dart
+++ mobile_app/lib/widgets/post.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:north/graphql/generated.graphql.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Post extends StatelessWidget {
   // todo: see if we can use a shared 'post' data fragment: https://github.com/comigor/artemis/issues/137#issuecomment-630383853
@@ -17,17 +18,33 @@ class Post extends StatelessWidget {
           ),
           // https://pub.dev/packages/carousel_slider for insta type thing?
           // https://medium.com/@nickwu241/creating-an-instagram-clone-with-flutter-4e81eab2ed69
-          Container(
-            height: 110,
-            child: Text(
-              '${post.haikus[0].line1}\n${post.haikus[0].line2}\n${post.haikus[0].line3}\n',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                height: 2,
-              ),
-            ),
-          )
+          CarouselSlider(
+              items: post.haikus.map((haiku) => Haiku(haiku: haiku)).toList(),
+              options: CarouselOptions(
+                viewportFraction: 1.0,
+                enableInfiniteScroll: false,
+              ))
         ],
+      ),
+    );
+  }
+}
+
+class Haiku extends StatelessWidget {
+  final HomePage$QueryRoot$Posts$Haikus haiku;
+
+  const Haiku({Key key, @required this.haiku}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 110,
+      child: Text(
+        '${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          height: 2,
+        ),
       ),
     );
   }
