d1401d68719f4b5a6512d6abfdfb68ad641f0b8a
zhammer <zachary_hammer@alumni.brown.edu>
2020-11-29 16:03:27 -0500

    make the carousel a bit nicer

--- mobile_app/lib/widgets/post.dart
+++ mobile_app/lib/widgets/post.dart
@@ -2,28 +2,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:north/graphql/generated.graphql.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class Post extends StatelessWidget {
+class Post extends StatefulWidget {
   // todo: see if we can use a shared 'post' data fragment: https://github.com/comigor/artemis/issues/137#issuecomment-630383853
   final HomePage$QueryRoot$Posts post;
 
   const Post({@required this.post});
 
   @override
+  _PostState createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
+  int _haikuIndex = 0;
+
+  void _onCarouselPageChanged(index, _) {
+    setState(() => _haikuIndex = index);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          Row(
-            children: [Text(post.createdBy.username)],
-          ),
           // https://pub.dev/packages/carousel_slider for insta type thing?
           // https://medium.com/@nickwu241/creating-an-instagram-clone-with-flutter-4e81eab2ed69
           CarouselSlider(
-              items: post.haikus.map((haiku) => Haiku(haiku: haiku)).toList(),
-              options: CarouselOptions(
-                viewportFraction: 1.0,
-                enableInfiniteScroll: false,
-              ))
+            items:
+                widget.post.haikus.map((haiku) => Haiku(haiku: haiku)).toList(),
+            options: CarouselOptions(
+              viewportFraction: 1.0,
+              enableInfiniteScroll: false,
+              onPageChanged: _onCarouselPageChanged,
+              height: 150,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (widget.post.haikus.length > 1)
+                CarouselIndicator(
+                  count: widget.post.haikus.length,
+                  index: _haikuIndex,
+                )
+            ],
+          )
         ],
       ),
     );
@@ -38,14 +60,51 @@ class Haiku extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 110,
-      child: Text(
-        '${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          height: 2,
+      child: Center(
+        child: Text(
+          '${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            height: 1.25,
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CarouselIndicator extends StatelessWidget {
+  final int count;
+  final int index;
+
+  CarouselIndicator({
+    @required this.count,
+    @required this.index,
+  });
+
+  Widget _buildDot(BuildContext context, {bool isActive}) {
+    final theme = CupertinoTheme.of(context);
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 3.0, right: 3.0),
+        child: Container(
+          height: isActive ? 7.5 : 6.0,
+          width: isActive ? 7.5 : 6.0,
+          decoration: BoxDecoration(
+            color: isActive ? theme.primaryColor : CupertinoColors.systemGrey,
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(count, (i) => i)
+          .map((i) => _buildDot(context, isActive: i == index))
+          .toList(),
     );
   }
 }
