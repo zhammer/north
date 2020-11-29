5c1a5387e0aa63d41d9149e7e3bb3398ed94b660
zhammer <zachary_hammer@alumni.brown.edu>
2020-11-29 16:16:38 -0500

    more work on haikus

--- mobile_app/lib/widgets/post.dart
+++ mobile_app/lib/widgets/post.dart
@@ -22,18 +22,25 @@ class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 20),
       child: Column(
         children: [
           // https://pub.dev/packages/carousel_slider for insta type thing?
           // https://medium.com/@nickwu241/creating-an-instagram-clone-with-flutter-4e81eab2ed69
-          CarouselSlider(
-            items:
-                widget.post.haikus.map((haiku) => Haiku(haiku: haiku)).toList(),
-            options: CarouselOptions(
-              viewportFraction: 1.0,
-              enableInfiniteScroll: false,
-              onPageChanged: _onCarouselPageChanged,
-              height: 150,
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(style: BorderStyle.solid),
+            ),
+            child: CarouselSlider(
+              items: widget.post.haikus
+                  .map((haiku) => Haiku(haiku: haiku))
+                  .toList(),
+              options: CarouselOptions(
+                viewportFraction: 1.0,
+                enableInfiniteScroll: false,
+                onPageChanged: _onCarouselPageChanged,
+                height: 150,
+              ),
             ),
           ),
           Row(
@@ -45,7 +52,7 @@ class _PostState extends State<Post> {
                   index: _haikuIndex,
                 )
             ],
-          )
+          ),
         ],
       ),
     );
@@ -62,7 +69,7 @@ class Haiku extends StatelessWidget {
     return Container(
       child: Center(
         child: Text(
-          '${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n',
+          '${haiku.line1}\n${haiku.line2}\n${haiku.line3}',
           textAlign: TextAlign.center,
           style: TextStyle(
             height: 1.25,
@@ -91,7 +98,7 @@ class CarouselIndicator extends StatelessWidget {
           height: isActive ? 7.5 : 6.0,
           width: isActive ? 7.5 : 6.0,
           decoration: BoxDecoration(
-            color: isActive ? theme.primaryColor : CupertinoColors.systemGrey,
+            color: isActive ? theme.primaryColor : CupertinoColors.inactiveGray,
             borderRadius: BorderRadius.circular(4.0),
           ),
         ),
